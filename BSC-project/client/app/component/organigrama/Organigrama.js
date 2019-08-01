7d598fa1b40f64f27825fd41f58d2390a66fd175
andresvr <avrz237@gmail.com>
2019-08-01 12:09:23 -0500

    add style and organizacional representation

--- BSC-project/client/app/component/organigrama/Organigrama.js
+++ BSC-project/client/app/component/organigrama/Organigrama.js
@@ -4,9 +4,18 @@ Template.Organigrama.helpers({
     organigrama: ()=>{
         return Organigrama.find({});
     },
-    gerencial:()=>{
-        return OrganigramaNivelTactico.find({});
+    departamentoLvl2:()=>{
+        return Departamento.find({organigramaLevel:'KZ9ZuaKJAqTL3qdLN-Gerencias Departamentales'});
+    },
+    departamentoLvl3:()=>{
+        return Departamento.find({organigramaLevel:'54ZQAZ8m8QZxTjDQv-Táctico'});
+    },
+    departamentoLvl4:()=>{
+        return Departamento.find({organigramaLevel:'j8tH4kzyjm677jZhg-Operativos'});
     }
+
+
+
 
 });
 
